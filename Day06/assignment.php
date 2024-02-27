c991a3f23b0f6e1f9b9411943bea3c2b1b04ce3b
Swan Yee Myo <swanyeemyo2001@gmail.com>
2024-02-27 09:21:34 +0630

    fourth commit

--- Day06/assignment.php
+++ Day06/assignment.php
@@ -1,7 +1,27 @@
-<?php 
- $title = $body = "";
- $titleErr = $bodyErr = "";
+<?php
+$title = $body = "";
+$titleErr = $bodyErr = "";
 
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    if (empty($_POST['title'])) {
+        $titleErr = 'Title is required';
+    } else {
+        $title = filter_var($_POST['title'], FILTER_SANITIZE_STRING);
+    }
+    
+    if (empty($_POST['body'])) {
+        $bodyErr = 'Body is required';
+    } else {
+        $body = filter_var($_POST['body'], FILTER_SANITIZE_STRING);
+    }
+    if(!empty($_POST['title'] && !empty($_POST['body']) )){
+        $persons = array('title'=>$title,'body' => $body);
+        $data = array_map(function($d){
+         return $d;
+        },$persons);
+        print_r($data);
+    }
+}
 ?>
 
 <!DOCTYPE html>
@@ -11,36 +31,27 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <script src="https://cdn.tailwindcss.com"></script>
-
 </head>
 <body>
     <div>
-        <form action="assignment.php" method="POST" class="max-w-lg p-3 rounded-md mx-auto" >
-        <div class="mb-5">
-    <label for="title" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your title</label>
-    <input type="text" id="title" name="title"  value="<?php echo $title  ?>"  class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" placeholder="name@flowbite.com" required />
-    </div>
-    <?php 
-             if($titleErr){ ?>
-                <span class="text-red-600" ><?php echo $titleErr;  ?> </span>
-           <?php }
-    ?>
-        <label for="message" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your message</label>
-        <textarea id="message"  name="body" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Leave a comment...">
-            <?php echo $body  ?>
-        </textarea>
-        <?php 
-            if($bodyErr){ ?>
-                <span class="text-red-600" ><?php echo $bodyErr  ?> </span>
-           <?php }
-        ?>
-        <div class="flex item-center justify-center
-        
-        ">
-            <input type="button" value="Submit" class="bg-black  text-white p-1 rounded-md" >
-        </div>
-
-    </form>
+        <form action="assignment.php" method="POST" class="max-w-lg p-3 shadow mt-16 rounded-md mx-auto">
+            <h5 class="text-2xl text-center font-mono font-semibold">Login Form</h5>
+            <div class="mb-5">
+                <label for="title" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your title</label>
+                <input type="text" id="title" name="title" value="<?php echo $title ?>" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" placeholder="name@flowbite.com" />
+                <?php if ($titleErr): ?>
+                    <span class="text-red-600"><?php echo $titleErr; ?></span>
+                <?php endif; ?>
+            </div>
+            <label for="message" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your message</label>
+            <textarea id="message" name="body" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Leave a comment..."><?php echo $body ?></textarea>
+            <?php if ($bodyErr): ?>
+                <span class="text-red-600"><?php echo $bodyErr; ?></span>
+            <?php endif; ?>
+            <div class="flex item-center justify-center my-5">
+                <input type="submit" value="Submit" class="bg-black text-white p-2 rounded-md">
+            </div>
+        </form>
     </div>
 </body>
 </html>
